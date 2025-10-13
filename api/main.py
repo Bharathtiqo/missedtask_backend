d5f6695a4f35cf4b2f9694db018feb6a850c9a54
Bharathtiqo <bharath.k@tiqo.co>
2025-10-13 14:51:48 +0530

    resolved cors

--- api/main.py
+++ api/main.py
@@ -66,7 +66,8 @@ app.add_middleware(
         "http://192.168.7.3:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "http://192.168.7.3:5173"
+        "http://192.168.7.3:5173",
+         "https://missedtask-frontend.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
